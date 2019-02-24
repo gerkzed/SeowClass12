e8c7289a16091c72e43e7e4255f3ecbe964b57c3
DeathyVoid <will.iams.sapo@gmail.com>
2019-02-23 23:29:10 -0500

    2nd commit

--- Seow12/src/Arrays_1.java
+++ Seow12/src/Arrays_1.java
@@ -1,14 +1,85 @@
+import java.io.*;
 import java.util.*;
 
 public class Arrays_1 {
-	public static void main (String args[]) {
+	public static void main (String args[]) throws FileNotFoundException {
 		
-		
-		
-		
+		int[] numList = readFiles();
+
+		int mode = calcMode(numList);
+
+		System.out.println(Arrays.toString(numList));
+		System.out.println(mode);
 		
 	}
-	
-	
-	
+
+	private static int calcMode(int[] numList) {
+		
+		int maxCount = 0, mode = 0;
+		
+		for (int i = 0; i < numList.length; i++) {
+			
+			int modeCounter = 0;
+			
+			for (int j = 0; j < numList.length; j++) {
+				
+				if (numList[i] == numList[j]) {
+					
+					modeCounter++;
+					
+				}
+			}
+			
+			if (modeCounter > maxCount) {
+				
+				maxCount = modeCounter;
+				mode = numList[i];
+				
+			}
+			
+		}
+
+		return mode;
+	}
+
+	private static int[] readFiles() {
+		
+		Scanner input;
+		Scanner arrayRead;
+
+    File f = new File("list.txt");
+		
+		try {
+			
+			input = new Scanner(f);
+			
+			int counter = 0;
+			
+			while (input.hasNextInt()) {
+				
+				counter++;
+				input.nextInt();
+				
+			}
+			
+			int[] numList = new int[counter];
+			
+			arrayRead = new Scanner(f);
+			
+			for (int i = 0; i < numList.length; i++) {
+				
+				numList[i] = arrayRead.nextInt();
+				
+			}
+			
+			return numList;
+			
+		} 
+		
+		catch (Exception e) {
+			
+			return null;
+			
+		}
+	}
 }
